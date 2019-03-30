463a58e8f6eccd2a44fcda9fab9d7e2f8fb615ee
Coursera Learner <nobody@nowhere.nul>
2019-03-30 05:15:52 +0000

    corrected another error

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -13,12 +13,12 @@ void assert_card_valid(card_t c) {
 
 const char * ranking_to_string(hand_ranking_t r) {
   switch(r){
-  case STRAIGHT_FLUSH: return "STRAIGHT FLUSH";
-  case FOUR_OF_A_KIND: return "FOUR OF A KIND";
-  case FULL_HOUSE: return "FULL HOUSE";
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+  case FULL_HOUSE: return "FULL_HOUSE";
   case FLUSH: return "FLUSH";
   case STRAIGHT: return "STRAIGHT";
-  case THREE_OF_A_KIND: return "THREE OF A KIND";
+  case THREE_OF_A_KIND: return "THREE_OF_ A_ KIND";
   case TWO_PAIR: return "TWO PAIR";
   case PAIR: return "PAIR";
   case NOTHING: return "NOTHING";

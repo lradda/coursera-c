2783bef28a46ed0b52d69f5c2c74f6a5b5683cba
Coursera Learner <nobody@nowhere.nul>
2019-03-30 05:17:35 +0000

    corrected another error

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -18,8 +18,8 @@ const char * ranking_to_string(hand_ranking_t r) {
   case FULL_HOUSE: return "FULL_HOUSE";
   case FLUSH: return "FLUSH";
   case STRAIGHT: return "STRAIGHT";
-  case THREE_OF_A_KIND: return "THREE_OF_ A_ KIND";
-  case TWO_PAIR: return "TWO PAIR";
+  case THREE_OF_A_KIND: return "THREE_OF_ A_KIND";
+  case TWO_PAIR: return "TWO_PAIR";
   case PAIR: return "PAIR";
   case NOTHING: return "NOTHING";
   }

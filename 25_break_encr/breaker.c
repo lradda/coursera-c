5f29505382623f627e48448816d369bec867ede0
Coursera Learner <nobody@nowhere.nul>
2019-05-25 00:20:37 +0000

    Assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -2,38 +2,74 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-//int freqCount (int letterNumber, FILE * f){
-// char c = fgetc(f);
-//  char currentLetter = c;
-//  int counter = 1;
-//  while ((c = fgetc(f)) != EOF){
-//    if (isalpha(c)){
-//     c = tolower(c);
-//      if (c == currentLetter){
-//	counter += 1;
-//      }
-//      if (c != currentLetter){
-
-
-int freqCount(int letterNumber, FILE * f){
+int freqCount (FILE * f){
+  int array[26] = {0};
   char c;
-  int counter = 0;
-  char letter = ('a' + letterNumber);
   while ((c = fgetc(f)) != EOF){
     if (isalpha(c)){
       c = tolower(c);
-      if (c == letter){
-	  counter += 1;
-	}
+      switch (c){
+      case 'a': array[0] += 1; break;
+      case 'b': array[1] += 1; break;
+      case 'c': array[2] += 1; break;
+      case 'd': array[3] += 1; break;
+      case 'e': array[4] += 1; break;
+      case 'f': array[5] += 1; break;
+      case 'g': array[6] += 1; break;
+      case 'h': array[7] += 1; break;
+      case 'i': array[8] += 1; break;
+      case 'j': array[9] += 1; break;
+      case 'k': array[10] += 1; break;
+      case 'l': array[11] += 1; break;
+      case 'm': array[12] += 1; break;
+      case 'n': array[13] += 1; break;
+      case 'o': array[14] += 1; break;
+      case 'p': array[15] += 1; break;
+      case 'q': array[16] += 1; break;
+      case 'r': array[17] += 1; break;
+      case 's': array[18] += 1;	break;
+      case 't': array[19] += 1; break;
+      case 'u': array[20] += 1; break;
+      case 'v': array[21] += 1; break;
+      case 'w': array[22] += 1; break;
+      case 'x': array[23] += 1; break;
+      case 'y': array[24] += 1; break;
+      case 'z': array[25] += 1;	break;
+      }
     }
   }
-  rewind(f);
-  return counter;
-}      
+  int largestIndex = 0;
+  int temp = 0;
+  for (int i = 0; i < 26; i++){
+    if (array[i] > temp){
+      temp = array[i];
+      largestIndex = i;
+    }
+  }
+  return largestIndex;
+}
+	
+
+
+//int freqCount(int letterNumber, FILE * f){
+//  char c;
+//  int counter = 0;
+//  char letter = ('a' + letterNumber);
+//  while ((c = fgetc(f)) != EOF){
+//    if (isalpha(c)){
+//      c = tolower(c);
+//      if (c == letter){
+//	  counter += 1;
+//	}
+//    }
+//  }
+//  rewind(f);
+//  return counter;
+//}      
  
 int main (int argc, char ** argv){
   if (argc != 2){
-    fprintf(stderr, "ppppUsage: encrypt key inputFileName\n");
+    fprintf(stderr, "Usage: encrypt key inputFileName\n");
     return EXIT_FAILURE;
   }
   FILE * f = fopen(argv[1], "r");
@@ -41,19 +77,19 @@ int main (int argc, char ** argv){
     perror("Could not open file");
     return EXIT_FAILURE;
   }
-  int letterCount;
-  int maxCount;
-  int index;
+  //int letterCount;
+  //int maxCount;
+  int index = freqCount(f);
   //array
   int key = 0;
-  for (int i = 0; i < 26; i++){
-    letterCount = freqCount(i, f);
-    if (letterCount > maxCount){
-      index = i;
-      maxCount = letterCount;
-    }
+  //for (int i = 0; i < 26; i++){
+  //letterCount = freqCount(i, f);
+  //if (letterCount > maxCount){
+  //  index = i;
+  //  maxCount = letterCount;
+  // }
     //letterCount[i] = freqCount(i, f);
-  }
+  //}
   //int temp = -1;
   //int mostCommon = 0;
   //for (int j = 0; j < 26; j++){
